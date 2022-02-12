9f66221de3884cb93febf677b22985e770f482a9
Jon AEY <54909917+JhonnyEkYam@users.noreply.github.com>
2022-02-12 02:31:54 -0500

    Interceptors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, Route, Router, RouterModule, Routes, UrlSegment } from '@angular/router';
+import { PreloadAllModules, Router, RouterModule, Routes, } from '@angular/router';
 import { httpInterceptorProviders } from './interceptors/auth.interceptor';
-import { Storage } from '@ionic/storage-angular';
-import { Drivers } from '@ionic/storage';
 // Guards
 import { AuthGuardGuard } from './guards/auth-guard.guard'
 import { IsAuthGuard } from './guards/is-auth.guard'
+import { StorageService } from './storage/storage.service';
 
 const routes: Routes = [
   {
@@ -37,14 +36,6 @@ const routes: Routes = [
     ],
     canActivateChild: [IsAuthGuard]
   },
-  // {
-  //   path: 'sign-in',
-  //   loadChildren: () => import('./auth/sign-in/sign-in.module').then( m => m.SignInPageModule)
-  // },
-  // {
-  //   path: 'sign-up',
-  //   loadChildren: () => import('./auth/sign-up/sign-up.module').then( m => m.SignUpPageModule)
-  // },
 ];
 
 @NgModule({
@@ -59,19 +50,13 @@ const routes: Routes = [
   ]
 })
 export class AppRoutingModule {
-  private storage: Storage;
   constructor(private router: Router) {
     (async ()=> {
       try{
-        this.storage = new Storage({
-          name: '__authcredentials',
-          driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage]
-        })
-        await this.storage.create()
-        let __ACCESS_TOKEN = await this.storage.get('ACCESS_TOKEN')
-        if(__ACCESS_TOKEN === null) router.navigateByUrl('auth/login');
+        let __ACCESS_TOKEN = await StorageService.getValue(StorageService.config.authKeys.__ACCESS_TOKEN)
+        if(__ACCESS_TOKEN === null) this.router.navigateByUrl('auth/login');
       } catch (error) {
-        router.navigateByUrl('auth/login')
+        this.router.navigateByUrl('auth/login')
       }
     })()
   }
--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -2,14 +2,22 @@ import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Router } from "@angular/router";
+import { StorageService } from "../storage/storage.service";
 
 @Injectable()
 
 class AuthInterceptor implements HttpInterceptor {
-    constructor(private router: Router){}
+    private authToken: string;
+    constructor(private router: Router) {
+        (async () => {
+            this.authToken = await StorageService.getValue(StorageService.config.authKeys.__ACCESS_TOKEN)
+        })()
+    }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log("Vamos a probar esto")
-        
+        // const authReq = req.clone({
+        //     headers: req.headers.set('Authorization', this.authToken)
+        // })
+        // return next.handle(authReq);
         return next.handle(req)
     }
 }
